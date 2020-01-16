cb28fcf25b889f6272301dbf22ea6fcf7ef9f3b7
sfedorov <serezhka@xakep.ru>
2020-01-16 19:52:46 +0300

    tcp forwarder

--- tcp-forwarder/src/main/java/com/github/serezhka/jap2s/tcpforwarder/TCPForwarderServer.java
+++ tcp-forwarder/src/main/java/com/github/serezhka/jap2s/tcpforwarder/TCPForwarderServer.java
@@ -1,9 +1,9 @@
-package com.github.serezhka.jap2s.jmuxer;
+package com.github.serezhka.jap2s.tcpforwarder;
 
 import com.github.serezhka.jap2s.receiver.handler.mirroring.MirrorDataConsumer;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInitializer;
@@ -11,12 +11,10 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.socket.ServerSocketChannel;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.handler.codec.http.HttpObjectAggregator;
-import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
-import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -25,7 +23,7 @@ import java.net.InetSocketAddress;
 @Slf4j
 @Component
 @ChannelHandler.Sharable
-public class JMuxerWebSocketServer extends SimpleChannelInboundHandler<BinaryWebSocketFrame> implements MirrorDataConsumer {
+public class TCPForwarderServer extends SimpleChannelInboundHandler<ByteBuf> implements MirrorDataConsumer {
 
     private final EventLoopGroup bossGroup;
     private final EventLoopGroup workerGroup;
@@ -33,13 +31,13 @@ public class JMuxerWebSocketServer extends SimpleChannelInboundHandler<BinaryWeb
 
     private ChannelHandlerContext ctx;
 
-    private ByteBuf mirrorData = ByteBufAllocator.DEFAULT.buffer();
-    private int nalus = 0;
+    @Value("${tcp.forwarder.port}")
+    private int port;
 
     @Autowired
-    public JMuxerWebSocketServer(EventLoopGroup bossGroup,
-                                 EventLoopGroup workerGroup,
-                                 Class<? extends ServerSocketChannel> serverSocketChannelClass) {
+    public TCPForwarderServer(EventLoopGroup bossGroup,
+                              EventLoopGroup workerGroup,
+                              Class<? extends ServerSocketChannel> serverSocketChannelClass) {
         this.bossGroup = bossGroup;
         this.workerGroup = workerGroup;
         this.serverSocketChannelClass = serverSocketChannelClass;
@@ -47,10 +45,6 @@ public class JMuxerWebSocketServer extends SimpleChannelInboundHandler<BinaryWeb
 
     @PostConstruct
     public void init() {
-
-        int port = 8081;
-        String path = "/ws";
-
         new Thread(() -> {
             ServerBootstrap serverBootstrap = new ServerBootstrap();
             try {
@@ -60,14 +54,10 @@ public class JMuxerWebSocketServer extends SimpleChannelInboundHandler<BinaryWeb
                         .childHandler(new ChannelInitializer<SocketChannel>() {
                             @Override
                             public void initChannel(final SocketChannel ch) {
-                                ch.pipeline().addLast(
-                                        new HttpServerCodec(),
-                                        new HttpObjectAggregator(65536),
-                                        new WebSocketServerProtocolHandler(path),
-                                        JMuxerWebSocketServer.this);
+                                ch.pipeline().addLast(TCPForwarderServer.this);
                             }
                         });
-                log.info("Starting websocket server on port: {}, path: {}", port, path);
+                log.info("Starting TCP socket server on port {}", port);
                 serverBootstrap.bind().sync().channel().closeFuture().sync();
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
@@ -76,7 +66,7 @@ public class JMuxerWebSocketServer extends SimpleChannelInboundHandler<BinaryWeb
     }
 
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, BinaryWebSocketFrame msg) {
+    protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) {
     }
 
     @Override
@@ -91,19 +81,14 @@ public class JMuxerWebSocketServer extends SimpleChannelInboundHandler<BinaryWeb
 
     @Override
     public void onData(byte[] data) {
-        mirrorData.writeBytes(data);
-        nalus++;
-        if (nalus > 30) {
-            nalus = 0;
-            sendData(mirrorData);
-            mirrorData = ByteBufAllocator.DEFAULT.buffer();
-        }
+        sendData(Unpooled.wrappedBuffer(data));
     }
 
     private void sendData(ByteBuf message) {
         if (ctx != null) {
             ctx.executor().execute(() -> ctx.writeAndFlush(new BinaryWebSocketFrame(message)));
-            // ctx.writeAndFlush(new BinaryWebSocketFrame(message));
+            //ctx.writeAndFlush(new BinaryWebSocketFrame(message));
         } else message.release();
     }
 }
+
